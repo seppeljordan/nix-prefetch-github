6d077038985f6a86f454178cdd41775556d48048
Sebastian Jordan <sebastian.jordan.mail@googlemail.com>
2019-09-02 06:05:46 +0200

    Move setup logic to setup.cfg

--- setup.py
+++ setup.py
@@ -1,13 +1,3 @@
 from setuptools import setup
 
-setup(
-    url='https://github.com/seppeljordan/nix-prefetch-github',
-    data_files=[
-        ('templates', [
-            'src/nix_prefetch_github/templates/prefetch-github.nix.j2',
-            'src/nix_prefetch_github/templates/nix-output.j2',
-            'src/nix_prefetch_github/VERSION',
-        ])
-    ],
-    include_package_data=True,
-)
+setup()
