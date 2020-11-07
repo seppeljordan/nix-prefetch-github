995e9a5fd5be6d162040cbf628b9e746d67f293a
Sebastian Jordan <sebastian.jordan.mail@googlemail.com>
2020-11-07 20:05:38 +0100

    release.sh: only create tag after version was uploaded to pypi

--- release.sh
+++ release.sh
@@ -7,7 +7,7 @@ VERSION=$(cat nix_prefetch_github/VERSION)
 ./clean
 ./test-prefetch
 ./clean
-git tag "v${VERSION}"
 python setup.py sdist
 twine upload "dist/nix-prefetch-github-${VERSION}.tar.gz"
+git tag "v${VERSION}"
 git push origin "v${VERSION}"
