0d3f3ca63e2ce13fa55ff7771783e554aae0d375
Sebastian Jordan <sebastian.jordan.mail@googlemail.com>
2021-07-01 00:01:40 +0200

    Update flake inputs

--- flake.nix
+++ flake.nix
@@ -2,7 +2,6 @@
   description = "nix-prefetch-github";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
     flake-compat = {
       url = "github:edolstra/flake-compat";
