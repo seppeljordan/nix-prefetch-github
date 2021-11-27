35ef4056fad3b8e572157d467d2c9079ae92877a
Sebastian Jordan <sebastian.jordan.mail@googlemail.com>
2021-11-27 01:53:16 +0100

    Refactor nix_prefetch_github.core.prefetch

--- nix_prefetch_github/core/prefetch.py
+++ nix_prefetch_github/core/prefetch.py
@@ -1,5 +1,6 @@
 import json
-from typing import Optional
+from dataclasses import dataclass
+from typing import Optional, cast
 
 from attr import attrib, attrs
 from effect import Effect
@@ -10,118 +11,139 @@ from nix_prefetch_github.templates import output_template
 from .effects import AbortWithErrorMessage, GetRevisionForLatestRelease, TryPrefetch
 from .error import AbortWithError
 from .hash import is_sha1_hash
+from .repository import GithubRepository
 from .revision_index import RevisionIndex
 from .url_hasher import PrefetchOptions, UrlHasher
 
 
-def revision_not_found_errormessage(repository, revision):
+def revision_not_found_errormessage(repository: GithubRepository, revision: str):
     return f"Revision {revision} not found for repository {repository.owner}/{repository.name}"
 
 
-def repository_not_found_error_message(repository):
+def repository_not_found_error_message(repository: GithubRepository):
     return f"Could not find release for {repository.owner}/{repository.name}"
 
 
+@dataclass(frozen=True)
 class _Prefetcher:
-    def __init__(
-        self, repository, url_hasher: UrlHasher, revision_index: RevisionIndex
+    url_hasher: UrlHasher
+    revision_index: RevisionIndex
+
+    @do
+    def prefetch_github(
+        self,
+        repository: GithubRepository,
+        prefetch: bool,
+        rev,
+        prefetch_options: PrefetchOptions,
     ):
-        self._repository = repository
-        self._revision_index = revision_index
-        self._prefetched_repository: Optional[PrefetchedRepository] = None
-        self._calculated_hash: Optional[str] = None
-        self._prefetch = None
-        self._revision: Optional[str] = None
-        self._fetch_submodules: Optional[bool] = None
-        self._url_hasher = url_hasher
-
-    @do
-    def prefetch_github(self, prefetch, rev, fetch_submodules):
-        self._prefetch = prefetch
-        self._revision = rev
-        self._fetch_submodules = fetch_submodules
-        yield self._detect_revision()
-        yield self._calculate_sha256_sum()
-        yield self._prefetch_repository()
-        return self._prefetched_repository
-
-    @do
-    def prefetch_latest_release(self, prefetch, fetch_submodules):
-        self._prefetch = prefetch
-        self._fetch_submodules = fetch_submodules
-        self._revision = yield Effect(
-            GetRevisionForLatestRelease(repository=self._repository)
-        )
-        if not self._revision:
-            yield Effect(
-                AbortWithErrorMessage(
-                    repository_not_found_error_message(self._repository)
-                )
-            )
-            raise AbortWithError()
-        yield self._calculate_sha256_sum()
-        yield self._prefetch_repository()
-        return self._prefetched_repository
-
-    @do
-    def _detect_revision(self):
-        actual_rev: Optional[str]
-        if isinstance(self._revision, str) and is_sha1_hash(self._revision):
-            actual_rev = self._revision
-        elif self._revision is None:
-            actual_rev = self._revision_index.get_revision_by_name(
-                self._repository, "HEAD"
-            )
-        else:
-            actual_rev = self._revision_index.get_revision_by_name(
-                self._repository, self._revision
-            )
-        if actual_rev is None:
+        revision = self._detect_revision(repository, rev)
+        if revision is None:
             yield Effect(
                 AbortWithErrorMessage(
                     message=revision_not_found_errormessage(
-                        repository=self._repository, revision=self._revision
+                        repository=repository, revision=rev
                     )
                 )
             )
             raise AbortWithError()
-        self._revision = actual_rev
+        return (
+            yield self._prefetch_github(
+                repository, prefetch, revision, prefetch_options
+            )
+        )
 
     @do
-    def _calculate_sha256_sum(self):
-        assert self._revision is not None
-        assert self._fetch_submodules is not None
-        self._calculated_hash = self._url_hasher.calculate_sha256_sum(
-            repository=self._repository,
-            revision=self._revision,
-            prefetch_options=PrefetchOptions(fetch_submodules=self._fetch_submodules),
+    def prefetch_latest_release(
+        self,
+        repository: GithubRepository,
+        prefetch: bool,
+        prefetch_options: PrefetchOptions,
+    ):
+        revision = yield Effect(GetRevisionForLatestRelease(repository=repository))
+        if not revision:
+            yield Effect(
+                AbortWithErrorMessage(repository_not_found_error_message(repository))
+            )
+            raise AbortWithError()
+        return (
+            yield self._prefetch_github(
+                repository, prefetch, revision, prefetch_options
+            )
         )
-        if not self._calculated_hash:
+
+    @do
+    def _prefetch_github(
+        self,
+        repository: GithubRepository,
+        prefetch: bool,
+        revision: str,
+        prefetch_options: PrefetchOptions,
+    ):
+        calculated_hash = self._calculate_sha256_sum(
+            repository, revision, prefetch_options
+        )
+        if calculated_hash is None:
             yield Effect(
                 AbortWithErrorMessage(
                     message=(
                         "Internal Error: Calculate hash value for sources "
-                        f"in github repo {self._repository.owner}/{self._repository.name}."
+                        f"in github repo {repository.owner}/{repository.name}."
                     )
                 )
             )
+        if prefetch:
+            yield self._prefetch_repository(
+                repository, revision, calculated_hash, prefetch_options
+            )
+        return PrefetchedRepository(
+            repository=repository,
+            sha256=calculated_hash,
+            rev=revision,
+            fetch_submodules=prefetch_options.fetch_submodules,
+        )
+
+    def _detect_revision(
+        self, repository: GithubRepository, revision: Optional[str]
+    ) -> Optional[str]:
+        actual_rev: Optional[str]
+        if isinstance(revision, str) and is_sha1_hash(revision):
+            actual_rev = revision
+        elif revision is None:
+            actual_rev = self.revision_index.get_revision_by_name(repository, "HEAD")
+        else:
+            actual_rev = self.revision_index.get_revision_by_name(
+                repository, cast(str, revision)
+            )
+        return actual_rev
+
+    def _calculate_sha256_sum(
+        self,
+        repository: GithubRepository,
+        revision: str,
+        prefetch_options: PrefetchOptions,
+    ) -> Optional[str]:
+        return self.url_hasher.calculate_sha256_sum(
+            repository=repository,
+            revision=revision,
+            prefetch_options=prefetch_options,
+        )
 
     @do
-    def _prefetch_repository(self):
-        if self._prefetch:
-            yield Effect(
-                TryPrefetch(
-                    repository=self._repository,
-                    sha256=self._calculated_hash,
-                    rev=self._revision,
-                    fetch_submodules=self._fetch_submodules,
-                )
+    def _prefetch_repository(
+        self,
+        repository: GithubRepository,
+        revision: str,
+        calculated_hash: str,
+        prefetch_options: PrefetchOptions,
+    ):
+        yield Effect(
+            TryPrefetch(
+                repository=repository,
+                sha256=calculated_hash,
+                rev=revision,
+                fetch_submodules=prefetch_options.fetch_submodules,
             )
-        self._prefetched_repository = PrefetchedRepository(
-            repository=self._repository,
-            sha256=self._calculated_hash,
-            rev=self._revision,
-            fetch_submodules=self._fetch_submodules,
         )
 
 
@@ -162,9 +184,12 @@ def prefetch_github(
     rev=None,
     fetch_submodules=True,
 ):
-    prefetcher = _Prefetcher(repository, url_hasher, revision_index)
+    prefetcher = _Prefetcher(url_hasher, revision_index)
     return prefetcher.prefetch_github(
-        prefetch=prefetch, rev=rev, fetch_submodules=fetch_submodules
+        repository=repository,
+        prefetch=prefetch,
+        rev=rev,
+        prefetch_options=PrefetchOptions(fetch_submodules=fetch_submodules),
     )
 
 
@@ -175,7 +200,9 @@ def prefetch_latest_release(
     prefetch,
     fetch_submodules,
 ):
-    prefetcher = _Prefetcher(repository, url_hasher, revision_index)
+    prefetcher = _Prefetcher(url_hasher, revision_index)
     return prefetcher.prefetch_latest_release(
-        prefetch=prefetch, fetch_submodules=fetch_submodules
+        repository=repository,
+        prefetch=prefetch,
+        prefetch_options=PrefetchOptions(fetch_submodules=fetch_submodules),
     )
