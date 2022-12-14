0c6e6e623469c7a4acf4847ae7c994403d06283d
Hraban Luyat <hraban@0brg.net>
2022-12-14 14:18:29 -0500

    fix: bake git into PATH at runtime

--- nix/nix-prefetch-github.nix
+++ nix/nix-prefetch-github.nix
@@ -7,6 +7,11 @@ in buildPythonPackage {
   outputs = [ "out" "doc" "man" ];
   src = ../.;
   nativeBuildInputs = [ sphinxHook sphinx-argparse ];
+  postInstall = ''
+    for f in $out/bin/* ; do
+        wrapProgram "$f" --prefix PATH : "${git}/bin"
+    done
+  '';
   checkInputs = [ git which pytestCheckHook ];
   sphinxBuilders = [ "singlehtml" "man" ];
   sphinxRoot = "docs";
