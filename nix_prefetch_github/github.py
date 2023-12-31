a17de169ea38c199015b09bf897d265ba96065fa
Sebastian Jordan <sebastian.jordan.mail@googlemail.com>
2023-12-31 02:39:56 +0100

    Increase logging for github client
    Before this change the logging done inside the github client was
    rather sparse. This change increases the logging output for log levels
    INFO and DEBUG slightly.

--- nix_prefetch_github/github.py
+++ nix_prefetch_github/github.py
@@ -17,13 +17,12 @@ class GithubAPIImpl:
     logger: Logger
 
     def get_tag_of_latest_release(self, repository: GithubRepository) -> Optional[str]:
+        self.logger.info(
+            f"Query latest release for repository {repository.owner}/{repository.name} from GitHub."
+        )
         url = f"https://api.github.com/repos/{repository.owner}/{repository.name}/releases/latest"
-        self.logger.debug("Query github api @ %s", url)
-        try:
-            with urlopen(url) as response:
-                response_json = self._decode_json_from_response(response)
-        except (HTTPError, JSONDecodeError) as e:
-            self.logger.error(e)
+        response_json = self._request_json_document(url)
+        if response_json is None:
             return None
         return response_json.get("tag_name")
 
@@ -31,18 +30,30 @@ class GithubAPIImpl:
         self, repository: GithubRepository, commit_sha1_hash: str
     ) -> Optional[datetime]:
         url = f"https://api.github.com/repos/{repository.owner}/{repository.name}/commits/{commit_sha1_hash}"
-        self.logger.debug("Query github api @ %s", url)
-        try:
-            with urlopen(url) as response:
-                response_json = self._decode_json_from_response(response)
-        except (HTTPError, JSONDecodeError) as e:
-            self.logger.error(e)
+        response_json = self._request_json_document(url)
+        if response_json is None:
             return None
         date_string = response_json.get("commit", {}).get("committer", {}).get("date")
+        return self._parse_timestamp(date_string)
+
+    def _parse_timestamp(self, timestamp: str) -> Optional[datetime]:
         try:
-            return datetime.strptime(date_string, "%Y-%m-%dT%H:%M:%S%z")
+            return datetime.strptime(timestamp, "%Y-%m-%dT%H:%M:%S%z")
         except ValueError as e:
             self.logger.exception(e)
+            return None
+
+    def _request_json_document(self, url: str) -> Optional[Any]:
+        self.logger.debug("GET JSON document from %s", url)
+        try:
+            with urlopen(url) as response:
+                self.logger.debug(
+                    "Response was %(status)s %(reason)s",
+                    dict(status=response.status, reason=response.reason),
+                )
+                return self._decode_json_from_response(response)
+        except (HTTPError, JSONDecodeError) as e:
+            self.logger.error(e)
             return None
 
     def _decode_json_from_response(self, response: HTTPResponse) -> Any:
